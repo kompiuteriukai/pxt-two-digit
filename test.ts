e859aeffd51f41602e6ec22696181e7c2c31335b
linclip <linclip@gmail.com>
2019-03-04 20:36:54 +0900

    0.2.1 add counter sample / test.js

--- test.ts
+++ test.ts
@@ -1,12 +1,24 @@
-basic.forever(() => {
+let item = 0
+input.onButtonPressed(Button.AB, function () {
+    item = 0
+    twodigit.showNumber(item)
+})
+input.onButtonPressed(Button.A, function () {
+    item += -1
+    if (item < 0) {
+        item = 0
+    }
+    twodigit.showNumber(item)
+})
+input.onButtonPressed(Button.B, function () {
+    item += 1
+    if (item > 99) {
+        item = 99
+    }
+    twodigit.showNumber(item)
+})
+item = 0
+twodigit.showNumber(item)
+basic.forever(function () {
 	
-})
-input.onButtonPressed(Button.A, () => {
-    twodigit.showNumber(12)
-})
-input.onButtonPressed(Button.B, () => {
-    twodigit.showNumber(34)
-})
-input.onButtonPressed(Button.AB, () => {
-    twodigit.showNumber(-9)
 })
